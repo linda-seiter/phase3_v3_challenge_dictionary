e0910feff4079caaf370986ea45cfd66b29ae8aa
Linda Seiter <linseiter@gmail.com>
2023-09-05 17:41:36 -0400

    wrap uuid as string to prevent dictionary key error

--- server/models.py
+++ server/models.py
@@ -1,16 +1,16 @@
 import uuid
 
-
 class Planet():
     """Planet model"""
 
     all = {}
 
     def __init__(self, name, distance_from_sun):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.name = name
         self.distance_from_sun = distance_from_sun
         type(self).all[self.id] = self  
+        
 
     def moons(self):
         """Get list of moons for this planet"""
@@ -23,7 +23,7 @@ class Moon():
     all = {}
 
     def __init__(self, name, orbital_period, planet_id):
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.name = name
         self.orbital_period = orbital_period
         self.planet_id = planet_id  
--- server/views/schemas.py
+++ server/views/schemas.py
@@ -4,22 +4,24 @@ from models import Planet
 
 
 class MoonSchema(Schema):
-    id = fields.UUID(dump_only=True)
+    id = fields.Str(dump_only=True)
     name = fields.Str(required=True, validate=Length(
         min=1, error="Must not be empty string."))
     orbital_period = fields.Float(required=True, validate=Range(min=0))
-    planet_id = fields.UUID(required=True)
+    planet_id = fields.Str(required=True)
 
     @validates("planet_id")
     def planet_foreign_key(self, value):
         """planet_id is valid foreign key"""
+        print(value)
+        print(Planet.all.keys())
         if Planet.all.get(value) is None:
             raise ValidationError(f"Foreign key violation for planet_id {value}.")
 
 
 class PlanetSchema(Schema):
 
-    id = fields.UUID(dump_only=True)
+    id = fields.Str(dump_only=True)
     name = fields.Str(required=True, validate=Length(
         min=1, error="Must not be empty string."))
     distance_from_sun = fields.Int(required=True, validate=Range(min=0))
